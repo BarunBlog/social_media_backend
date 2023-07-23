679ad3aacb0c2a11338173353270a29ad457e913
BarunBlog <bhattacharjeebarun25@gmail.com>
2023-07-23 21:05:53 +0600

    Changed cors settings

--- index.js
+++ index.js
@@ -35,14 +35,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        console.log(origin);
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: allowedOrigins
 }));
 
 
